f9e815d07f97ad6b1a1c6feae729b57899cf934d
jah09 <jasuhot1@gmail.com>
2025-03-16 16:10:03 +0800

    feat: working on pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
+use App\Livewire\CartPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductDetailPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+
+Route::get('/checkout', CheckoutPage::class);
+// Route::prefix('categories')->group(function () {
+  
+//     Route::get('/categories-page', CategoriesPage::class);
+//     Route::get('/products-page', ProductsPage::class);
+// });
