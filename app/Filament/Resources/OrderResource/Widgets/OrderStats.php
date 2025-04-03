0030ed7f1418ed331fecfadfc5918f2f58b7436d
Jah <81201468+jah09@users.noreply.github.com>
2025-04-03 22:23:04 +0800

    fix: added default value (#5)
    * feat: worked on dynamic style in navbar and implemented wire: navigate
    
    * fix: added default value

--- app/Filament/Resources/OrderResource/Widgets/OrderStats.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -12,10 +12,10 @@ class OrderStats extends BaseWidget
     protected function getStats(): array
     {
         return [
-           Stat::make('New Orders',Order::query()->where('status', 'new')->count()),
-           Stat::make('Order Processing',Order::query()->where('status', 'processing')->count()),
-           Stat::make('Order Shipped',Order::query()->where('status', 'shipped')->count()),
-           Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'), 'PHP')), 
+            Stat::make('New Orders', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count()),
+            Stat::make('Order Shipped', Order::query()->where('status', 'shipped')->count()),
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total') ?? 0, 'PHP')),
         ];
     }
 }
