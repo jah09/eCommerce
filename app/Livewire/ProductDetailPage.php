9c0d931b587e100f7030992fa861f12bffd29c97
jah09 <jasuhot1@gmail.com>
2025-05-15 21:25:53 +0800

    Refactor product page to dynamically display categories and brands from the database; update product links to use slugs instead of IDs in routes.

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+
+#[Title('Product Detail Nin-ja')]
 
 class ProductDetailPage extends Component
 {
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    public function hydrate()
+    {
+        // $this->product = Product::where('slug', $this->slug)->firstOrFail();
+    }
     public function render()
     {
-        return view('livewire.product-detail-page');
+        // 
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail()
+        ]);
     }
 }
--- app/Livewire/ProductPage.php
+++ app/Livewire/ProductPage.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Product Page Nin-ja')]
 class ProductPage extends Component
 {
+    use \Livewire\WithPagination;
+
     public function render()
     {
-        return view('livewire.product-page');
+        $productQuery = Product::query()->where('is_active', 1)->paginate(4);
+
+        return view('livewire.product-page', [
+            'products' => $productQuery,
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug', 'image']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug', 'image']),
+
+        ]);
     }
 }
